8202984803055359a1932b6893268556fc48e53e
KarolChang <super000999888@gmail.com>
2021-12-20 12:06:37 +0800

    add log route

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,10 @@ const router = express.Router()
 
 const record = require('./modules/record')
 const close = require('./modules/close')
+const log = require('./modules/log')
 
 router.use('/record', record)
 router.use('/close', close)
+router.use('/log', log)
 
 module.exports = router
---
+++ routes/modules/log.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const router = express.Router()
+
+const db = require('../../models')
+const Log = db.Log
+
+// read
+router.get('/all', async (req, res, next) => {
+  try {
+    const logs = await Log.findAll({
+      order: [['createdAt', 'DESC']]
+    })
+    return res.json(logs)
+  } catch (error) {
+    return next(error)
+  }
+})
+
+module.exports = router
