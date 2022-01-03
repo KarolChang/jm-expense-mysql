9d74d304203088875bb08eef3dab7ad7aaa71f64
KarolChang <super000999888@gmail.com>
2022-01-03 09:52:30 +0800

    add lineBot api

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,11 @@ const router = express.Router()
 const record = require('./modules/record')
 const close = require('./modules/close')
 const log = require('./modules/log')
+const lineBot = require('./modules/lineBot')
 
 router.use('/record', record)
 router.use('/close', close)
 router.use('/log', log)
+router.use('/lineBot', lineBot)
 
 module.exports = router
