0ea34c078e912649e5ed40e085c47c2daf6833bd
KarolChang <super000999888@gmail.com>
2022-01-14 16:14:48 +0800

    deploy

--- routes/modules/lineBot.js
+++ routes/modules/lineBot.js
@@ -3,22 +3,22 @@ const router = express.Router()
 const axios = require('axios')
 
 // push message
-router.post('/push', async (req, res, next) => {
-  try {
-    const lineBotURL = 'https://api.line.me/v2/bot/message/multicast'
-    const apiHelperLineBot = axios.create({
-      baseURL: lineBotURL
-    })
-    apiHelperLineBot.defaults.headers.post['Content-Type'] = 'application/json'
-    apiHelperLineBot.defaults.headers.common['Authorization'] = 'Bearer ' + process.env.CHANNEL_ACCESS_TOKEN
-    await apiHelperLineBot.post('/', req.body)
-    return res.json({ message: 'line message push success', input: req.body })
-  } catch (error) {
-    return next(error)
-  }
-})
+// router.post('/push', async (req, res, next) => {
+//   try {
+//     const lineBotURL = 'https://api.line.me/v2/bot/message/multicast'
+//     const apiHelperLineBot = axios.create({
+//       baseURL: lineBotURL
+//     })
+//     apiHelperLineBot.defaults.headers.post['Content-Type'] = 'application/json'
+//     apiHelperLineBot.defaults.headers.common['Authorization'] = 'Bearer ' + process.env.CHANNEL_ACCESS_TOKEN
+//     await apiHelperLineBot.post('/', req.body)
+//     return res.json({ message: 'line message push success', input: req.body })
+//   } catch (error) {
+//     return next(error)
+//   }
+// })
 
-router.post('/push/new', async (req, res, next) => {
+router.post('/push', async (req, res, next) => {
   try {
     const lineBotURL = 'https://linebot20220114.herokuapp.com'
     const apiHelperLineBot = axios.create({
