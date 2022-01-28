048e93a1df85d35c7a36e43eed8677ba4634d8f7
KarolChang <super000999888@gmail.com>
2022-01-28 11:56:55 +0800

    deploy

--- routes/modules/lineBot.js
+++ routes/modules/lineBot.js
@@ -2,22 +2,7 @@ const express = require('express')
 const router = express.Router()
 const axios = require('axios')
 
-// push message
-// router.post('/push', async (req, res, next) => {
-//   try {
-//     const lineBotURL = 'https://api.line.me/v2/bot/message/multicast'
-//     const apiHelperLineBot = axios.create({
-//       baseURL: lineBotURL
-//     })
-//     apiHelperLineBot.defaults.headers.post['Content-Type'] = 'application/json'
-//     apiHelperLineBot.defaults.headers.common['Authorization'] = 'Bearer ' + process.env.CHANNEL_ACCESS_TOKEN
-//     await apiHelperLineBot.post('/', req.body)
-//     return res.json({ message: 'line message push success', input: req.body })
-//   } catch (error) {
-//     return next(error)
-//   }
-// })
-
+// push messages
 router.post('/push', async (req, res, next) => {
   try {
     const lineBotURL = 'http://linebot20220114.herokuapp.com'
